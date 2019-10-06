5a6d53015415cba8be7c5f5675a2d392ef06fcb5
PrestonGarcia <prestongarcia77@gmail.com>
2019-10-05 20:55:51 -1000

    Added more errors to the program.

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -8,18 +8,23 @@
 void switchNumbers(int array[], int firstIndex, int secondIndex); //Declaration
 int main()
 {//Start bracket for main function
-    int ourUnsortedList[] = {7, 4, 3, 8}; 
+	int firstNumber;
+	int secondNumber;
+	int thirdNumber;
+	int fourth;
+    int ourUnsortedList[] = {firstNumber, secondNumber, thirdNumber, fourth};
+	cin >> firstNumber;
+	cin >> secondNumber;
+	cin >> thirdNumber;
+	cin >> fourth;
     int stuff = sizeof(ourUnsortedList) / sizeof(*ourUnsortedList);
-    for (int i = 0; i < stuff; i++)
-    { //Start bracket for for loop counting how many iterations through list
-        for (int y = 0; y < stfuf; i++)
-        { //Start bracket for for loop iterating through each list element
-            if (ourUnsortedList[y] > ourUnsortedList[y+1])
-            {//Start brackets for if statement comparing list values
-                switchNumbers(ourUnsortedList, y, y+1); //links to function on line 27
-            }//End bracket for if statement comparing list values
-        }//End bracket for for loop iterating through each list element
-    }//End bracket for for loop counting how many iterations through list
+    for (int y = 0; y < stfuf; i++)
+    { //Start bracket for for loop iterating through each list element
+        if (ourUnsortedList[y] > ourUnsortedList[y+1])
+        {//Start brackets for if statement comparing list values
+			switchNumbers(ourUnsortedList, y, y+1); //links to function on line 27
+        }//End bracket for if statement comparing list values
+    }//End bracket for for loop iterating through each list element
     return 0;
 
 void switchNumbers(int array[], int firstIndex, int secondIndex)
